143070cf1a1cb2549d246ebde363e5f4b6476d59
Fernando Hurtado <rockfercho@gmail.com>
2020-04-23 21:47:38 -0400

    remove commetn line

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 async function connect() {
-  //mongoose.connect('mongodb://username:password@host:port/database?options...', {useNewUrlParser: true});
   await mongoose.connect('mongodb://localhost:27017/mysystem', { useUnifiedTopology: true, useNewUrlParser: true });
   console.log('Data base connected ...');
 }
